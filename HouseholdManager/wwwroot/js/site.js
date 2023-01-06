075c326e4a8e97dbe5cb508e844105f39213e317
narhiril <roxannarusbarsky@yahoo.com>
2023-01-06 01:31:44 -0600

    Theme choice should persist now

--- HouseholdManager/wwwroot/js/site.js
+++ HouseholdManager/wwwroot/js/site.js
@@ -2,31 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-const storedTheme = localStorage.getItem('theme');
-const html = document.documentElement;
-
-function getPreferredTheme() {
-    if (storedTheme) {
-        return storedTheme;
-    } else {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    }
-}
-
-function setTheme(theme) {
-    if (theme === 'dark') {
-        html.setAttribute('data-bs-theme', 'dark');
-    } else {
-        html.removeAttribute('data-bs-theme');
-    }
-}
-
-function setDefaultTheme() {
-    setTheme(getPreferredTheme);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    setDefaultTheme();
-    console.log(`Theme loaded (${getPreferredTheme()})`);
-});
