113ed276bc02897066da3ec5f59b44ab49254af9
narhiril <roxannarusbarsky@yahoo.com>
2023-01-09 22:44:06 -0600

    Consistency pass for property names

--- HouseholdManager/Models/ViewModels/ThemePickerViewModel.cs
+++ HouseholdManager/Models/ViewModels/ThemePickerViewModel.cs
@@ -2,15 +2,14 @@
 {
     public class ThemePickerViewModel
     {
-        //These properrties must have these exact names for Syncfusion
-        public string? ID { get; set; }
+        public string? Id { get; set; }
         public string? Text { get; set; }
 
         public List<ThemePickerViewModel> GetThemes()
         {
             List<ThemePickerViewModel> themeList = new List<ThemePickerViewModel>();
-            themeList.Add(new ThemePickerViewModel { ID = "bootstrap5", Text = "Light Mode" });
-            themeList.Add(new ThemePickerViewModel { ID = "bootstrap5-dark", Text = "Dark Mode" });
+            themeList.Add(new ThemePickerViewModel { Id = "bootstrap5", Text = "Light Mode" });
+            themeList.Add(new ThemePickerViewModel { Id = "bootstrap5-dark", Text = "Dark Mode" });
             return themeList;
         }
     }
