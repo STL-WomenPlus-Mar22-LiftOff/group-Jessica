20de66785189e6a5a86f515e228b86de2e5ef1c3
wilsons6561 <wilsons6561@users.noreply.github.com>
2023-01-10 20:34:49 -0600

    Added DueDate to "Mission" Model, updated corresponding views

--- HouseholdManager/Controllers/MissionController.cs
+++ HouseholdManager/Controllers/MissionController.cs
@@ -55,7 +55,7 @@ namespace HouseholdManager.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MissionId,MissionName,RoomId,Point")] Models.Mission mission)
+        public async Task<IActionResult> Create([Bind("MissionId,MissionName,RoomId,Point,DueDate")] Models.Mission mission)
         {
             if (ModelState.IsValid)
             {
@@ -89,7 +89,7 @@ namespace HouseholdManager.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("MissionId,MissionName,RoomId,Point")] Models.Mission mission)
+        public async Task<IActionResult> Edit(int id, [Bind("MissionId,MissionName,RoomId,Point,DueDate")] Models.Mission mission)
         {
             if (id != mission.MissionId)
             {
