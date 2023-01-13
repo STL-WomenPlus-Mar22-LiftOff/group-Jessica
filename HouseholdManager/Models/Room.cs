7262083251a6254ede39494c13848186fec16822
narhiril <roxannarusbarsky@yahoo.com>
2023-01-13 01:02:59 -0600

    Delete erroneous using statement
    Signed-off-by: narhiril <roxannarusbarsky@yahoo.com>

--- HouseholdManager/Models/Room.cs
+++ HouseholdManager/Models/Room.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
 
 namespace HouseholdManager.Models
 {
