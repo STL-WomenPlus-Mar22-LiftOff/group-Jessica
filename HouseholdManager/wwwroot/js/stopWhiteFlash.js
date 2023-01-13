30068d94fd12f7a6723e1a68f4e4a7515ca03814
narhiril <roxannarusbarsky@yahoo.com>
2023-01-13 00:27:58 -0600

    Documentation

--- HouseholdManager/wwwroot/js/stopWhiteFlash.js
+++ HouseholdManager/wwwroot/js/stopWhiteFlash.js
@@ -1,4 +1,5 @@
-﻿(function () {
+﻿//This is a fast check to prevent an irritating white flash on page load when set to dark mode
+(function () {
     const userTheme = localStorage.getItem('user-theme');
     const topElement = document.documentElement;
     if (userTheme !== 'light') {
