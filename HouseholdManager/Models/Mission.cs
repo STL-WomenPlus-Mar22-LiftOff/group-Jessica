b0c4c38fbd3cc0f8cc2ed1322ae6b1854f41de0a
Alli Kellerman <kellermanalli@gmail.com>
2023-01-15 16:44:43 -0600

    updated form headings on Mission/Create form

--- HouseholdManager/Models/Mission.cs
+++ HouseholdManager/Models/Mission.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace HouseholdManager.Models
 {
@@ -10,18 +11,22 @@ namespace HouseholdManager.Models
         public int MissionId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [DisplayName("Name")]
         [Required(ErrorMessage = "Name of mission is required.")]
         public string MissionName { get; set; }
 
         //RoomId-Foreign Key
         [Range(1, int.MaxValue, ErrorMessage = "Please select a room")]
+        [DisplayName("Room")]
         public int RoomId { get; set; }
 
         public Room? Room { get; set; }
 
         [Range(1, 5, ErrorMessage = "Amount should be greater than zero and no more than five.")]
+        [DisplayName("Difficulty")]
         public int Point { get; set; }
 
+        [DisplayName("Due Date")]
         public DateTime DueDate { get; set; }
 
     }
