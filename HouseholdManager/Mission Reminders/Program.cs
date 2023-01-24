fa3eb527ac8731a17bfec1fbc255931827b8cbee
Erica <johnson.ericastarr@gmail.com>
2023-01-23 19:23:52 -0600

    twilio

--- HouseholdManager/Mission Reminders/Program.cs
+++ HouseholdManager/Mission Reminders/Program.cs
@@ -1,6 +1,25 @@
-﻿namespace HouseholdManager.Mission_Reminders
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "Hi there",
+            from: new Twilio.Types.PhoneNumber("+15017122661"),
+            to: new Twilio.Types.PhoneNumber("+15558675310")
+        );
+
+        Console.WriteLine(message.Sid);
     }
 }
