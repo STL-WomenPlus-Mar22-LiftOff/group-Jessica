da0f5183d8841004f236144ac48d7c26cd1a3c49
Erica <johnson.ericastarr@gmail.com>
2023-01-25 14:40:48 -0600

    Reminders added classes

--- HouseholdManager/Mission Reminders/MissionReminder.cs
+++ HouseholdManager/Mission Reminders/MissionReminder.cs
@@ -1,6 +1,30 @@
-﻿namespace HouseholdManager.Mission_Reminders
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace HouseholdManager.Mission_Reminders
 {
     public class MissionReminder
     {
+        public class MissionReminder
+        {
+            public static int ReminderTime = 30;
+            public int Id { get; set; }
+
+            [Required]
+            public string MemberName { get; set; }
+
+            [Required, Phone, Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            public DateTime Time { get; set; }
+
+            [Required]
+            public string Timezone { get; set; }
+
+            [Display(Name = "Created at")]
+            public DateTime CreatedAt { get; set; }
+        }
     }
+}
 }
