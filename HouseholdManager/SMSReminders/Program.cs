5ddd6dd2c644bf3121d0f5b13a5375fa87795d68
Erica <johnson.ericastarr@gmail.com>
2023-01-25 16:44:03 -0600

    simple reminder

---
+++ HouseholdManager/SMSReminders/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace HouseholdManager.SMSReminders
+// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        TwilioClient.Init(accountSid, authToken);
+
+    var message = MessageResource.Create(
+        /*messagingServiceSid: "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        body: "This is a scheduled message",
+        sendAt: new DateTime(2021, 11, 30, 20, 36, 27),
+        scheduleType: MessageResource.ScheduleTypeEnum.Fixed,
+        statusCallback: new Uri("https://webhook.site/xxxxx"),
+        to: new Twilio.Types.PhoneNumber("+16362883683")*/
+    body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+            from: new Twilio.Types.PhoneNumber("+19288822706"),
+            to: new Twilio.Types.PhoneNumber("+6362883683")
+        );
+
+        Console.WriteLine(message.Sid);
+    }
+}
